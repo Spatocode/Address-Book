94caa7d792c8c8f601c4a7ea5ac94a351f1d00c8
Spatocode <impulseofficial069@gmail.com>
2018-09-03 18:31:25 +0100

    Declared global vars

---
+++ scripts/ab.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+const trHead = document.querySelector('#head');
+const conData = document.querySelector('#data');
+const nameInput = document.querySelector('#name');
+const addInput = document.querySelector('#add');
+const telInput = document.querySelector('#tel');
+const emailInput = document.querySelector('#email');
+const urlInput = document.querySelector('#url');
+const button = document.querySelector('button');
+
+let db
